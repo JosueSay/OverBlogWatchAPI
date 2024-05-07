46c075e53f27ca6a92dc20c59fe5a823354d69c2
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-05-07 12:53:44 -0600

    test: fixing ambiguity of environment variables

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'mypassword'
-export const DB_NAME = process.env.DB_NAME || 'mydb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_HOST = process.env.VE_HOST || 'localhost'
+export const DB_USER = process.env.VE_USER || 'root'
+export const DB_PASSWORD = process.env.VE_PASSWORD || 'mypassword'
+export const DB_NAME = process.env.VE_NAME || 'mydb'
+export const DB_PORT = process.env.VE_PORT || 3306
